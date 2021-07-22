03fe256cb4dfdf48f5534afdb820bab2a174e781
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-07-22 12:52:43 +0530

    Create CreateGroup, SingleGroup, ListGroups views for groups app

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import(
+    LoginRequiredMixin,
+    PermissionRequiredMixin
+)
+from django.urls import reverse
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
+from django.views import generic
+from groups.models import Group, GroupMember
+from . import models
 
-# Create your views here.
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ("name", "description")
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
