d4ceeb2ab9b55160f4cdeb5bff90c6515f769d87
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-07-22 15:46:09 +0530

    Add all the apps to settings.py

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
-    'accounts'
+    'accounts',
+    'groups',
+    'posts'
 ]
 
 MIDDLEWARE = [
